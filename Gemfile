2073985fb4ff6cffd106b9a6fdae533c11cede4c
elifoster <elifosterwy@gmail.com>
2016-01-23 23:47:58 -0800

    :white_check_mark: Implement Travis

--- Gemfile
+++ Gemfile
@@ -3,3 +3,8 @@ source 'https://rubygems.org'
 ruby '2.3.0'
 
 gem('httpclient', '>= 2.7.1')
+
+group :test do
+  gem 'rake'
+  gem 'minitest'
+end
---
+++ test.rb
@@ -0,0 +1,18 @@
+require_relative 'lib/isgd'
+require 'minitest/autorun'
+
+describe 'ISGD' do
+  rubygems = 'https://rubygems.org'.freeze
+  url = ISGD.shorten(rubygems).freeze
+
+  it 'shortens a URL' do
+    url.must_be_instance_of(String)
+    url.must_include('http://is.gd/')
+  end
+
+  it 'looks up a shortened URL' do
+    lookup = ISGD.lookup(url)
+    lookup.must_be_instance_of(String)
+    assert_equal(lookup, rubygems)
+  end
+end
